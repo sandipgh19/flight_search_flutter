c53fcf87997d16dc361da3e10cb3cf8c6f11fb15
sandipghosh <sandipgh19@gmail.com>
2019-01-09 21:53:06 +0530

    flight tickets

---
+++ lib/ticket/flight_stop_ticket.dart
@@ -0,0 +1,10 @@
+class FlightStopTicket {
+  String fromLocation;
+  String fromShort;
+  String toLocation;
+  String toShort;
+  String flightNumber;
+
+  FlightStopTicket(
+      this.fromLocation, this.fromShort, this.toLocation, this.toShort, this.flightNumber);
+}
